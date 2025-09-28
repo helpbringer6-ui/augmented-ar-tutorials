a0650bc4a4eacd03215d43494cad9a910886fc18
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-28 08:30:44 -0500

    Updated welcome message to include additional context.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Main file for the application
 
-console.log('Welcome to the Augmented Reality Tutorials!');
+console.log('Welcome to the Augmented Reality Tutorials! Get ready to explore!');
 // Add your AR logic here. Implement initialization and event listeners.
