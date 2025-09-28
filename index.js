e2c39f70bae996a2f3650e3d24b8b7b96145e934
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-28 08:28:34 -0500

    Update welcome message for clarity

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Hauptdatei für die Anwendung
 
-console.log('Willkommen zu den Augmented AR Tutorials!');
+console.log('Willkommen zu den Augmented Reality Tutorials!');
 // Fügen Sie hier Ihre AR-Logik hinzu
