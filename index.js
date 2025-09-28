ca8d7170c6abc307e577a9320c69e161a78cee91
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-28 08:30:23 -0500

    Add a brief description for the AR logic section.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Main file for the application
 
 console.log('Welcome to the Augmented Reality Tutorials!');
-// Add your AR logic here
+// Add your AR logic here. Implement initialization and event listeners.
